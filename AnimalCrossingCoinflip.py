eabac2fe5dbe46b251d97e925ce0ca5a2dd9f574
jts921 <jordan.schultz921@gmail.com>
2020-03-21 19:41:50 -0700

    Add files via upload

---
+++ AnimalCrossingCoinflip.py
@@ -0,0 +1,12 @@
+#random number generator (rng)
+import random
+for x in range (1):
+    x = int(random.randint(1,2))
+
+print("Should Erickson buy animal crossing digitally?")
+
+if(x == 1):
+    print("buy animal crossing digitally")
+
+if(x == 2):
+    print("wait for amazon delivery")
