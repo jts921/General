1425f8bbdd5fa8fcf05b8a1e752917e895046ed7
jts921 <jordan.schultz921@gmail.com>
2020-04-05 15:00:12 -0700

    madlib

---
+++ MadLibsGenerator.py
@@ -0,0 +1,21 @@
+print("Time to play Mad Libs")
+
+print("Type a verb (present)")
+x = input()
+#print(x)
+
+print("Type a noun (plural)")
+y = input()
+#print(y)
+
+print("Type an adjective")
+z = input()
+#print(z)
+
+print("I am")
+print(x)
+print("to the store to buy some")
+print(y)
+print("for my")
+print(z)
+print("Family")
