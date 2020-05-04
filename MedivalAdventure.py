4a8834ef9a0534cd2d273f9dd641960d6d4d0650
jts921 <jordan.schultz921@gmail.com>
2020-05-03 18:31:34 -0700

    Adding games to file

---
+++ MedivalAdventure.py
@@ -0,0 +1,27 @@
+print("Welcome to the your Adventure")
+print("Type 'Start' to begin")
+
+y = input()
+
+if(y == "Start"):
+    print("Please choose your class")
+
+#print("- Mage")
+#print("- Warrior")
+#print("- Marksman")
+#print("- Jester")
+
+#x = input()
+
+
+#rando()
+
+
+
+
+
+#random number generator (rng)
+#def rando ():
+#    import random
+#    for x in range (1):
+#        print (random.randint(1,6))
---
+++ TextAdventure.py
@@ -0,0 +1,19 @@
+print("Welcome to the your Adventure\nType 'Start' to begin")
+
+y = input()
+
+if(y == "Start"):
+    print("You enter a dungeon with three directions of travel.\nWhich path do you chose? (choose number)Sa")
+
+print("1. North\n2. East\n3. West")
+
+x = input()
+
+if(x == "1"):
+    print("You encounter a narrow path")
+
+if(x == "2"):
+    print("fuck")
+
+if(x == "3"):
+    print("balls")
